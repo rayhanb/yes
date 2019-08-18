fec066112a2a0771945c45fa4c4daa93e1f130dd
rayhanb <rborthakur@icloud.com>
2019-08-18 11:42:24 -0700

    Update binarysearch.js

--- binarysearch.js
+++ binarysearch.js
@@ -3,9 +3,11 @@
 var doSearch = function(array, targetValue) {
 	var min = 0;
 	var max = array.length - 1;
-    var guess;
+    var guess; 
+    
   while(max >= min){
         guess = Math.floor((max+min)/2);
+        println("guess: " + guess);
         if(array[guess]===targetValue){return guess;}
         else if(array[guess]<targetValue){min=guess+1;}
         else{max=guess-1; }
@@ -17,7 +19,10 @@ var doSearch = function(array, targetValue) {
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-var result = doSearch(primes, 73);
+var result = doSearch(primes, 73); 
+
 println("Found prime at index " + result);
 
 Program.assertEqual(doSearch(primes,73),20);
+
+ 
